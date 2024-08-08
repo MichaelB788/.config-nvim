76c7dcf8ab18880ead1e9060ddd515ab7c33b7d3
Michael Barrientos <michaelbarrientos788@gmail.com>
2024-08-07 20:43:15 -0400

    Start of repository

---
+++ init.vim
@@ -0,0 +1,6 @@
+lua require("plugins")
+lua require("mike.core.options")
+lua require("mike.core.keymaps")
+lua require("mike.core.colorscheme")
+
+
---
+++ lua/mike/core/keymaps.lua
@@ -0,0 +1,45 @@
+-- For help on key notation, type :h key-notation
+
+vim.g.mapleader = " "
+local builtin = require('telescope.builtin')
+
+-- Yank into system clipboard
+vim.keymap.set({'n', 'v'}, '<leader>y', '"+y') -- yank motion
+vim.keymap.set({'n', 'v'}, '<leader>Y', '"+Y') -- yank line
+
+vim.keymap.set({'n', 'v'}, '<leader>a', ':%y+<Enter>') -- yank all
+
+-- Delete into system clipboard
+vim.keymap.set({'n', 'v'}, '<leader>d', '"+d') -- delete motion
+vim.keymap.set({'n', 'v'}, '<leader>D', '"+D') -- delete line
+
+-- Paste from system clipboard
+vim.keymap.set('n', '<leader>p', '"+p')  -- paste after cursor
+vim.keymap.set('n', '<leader>P', '"+P')  -- paste before cursor
+
+-- Enter explorer GUI
+vim.keymap.set('n', '<leader>e', ':Ex<Enter>') -- Open Explorer in current tab
+vim.keymap.set('n', '<leader>t', ':tabnew | :Ex <Enter>') -- Open Explorer in a new tab
+
+-- Write and Quit
+vim.keymap.set({'n', 'v'}, '<leader>w', ':wall<Enter>') -- Write all
+vim.keymap.set({'n', 'v'}, '<leader>q', ':q!<Enter>') -- Write and close the current tab
+
+-- Autocomplete
+vim.keymap.set('i', '<C- >', '<C-p>')
+
+-- Insert Mode commands
+vim.keymap.set('i', '<C-h>', '<Left>') -- Move cursor left
+vim.keymap.set('i', '<C-j>', '<Down>') -- Move cursor down
+vim.keymap.set('i', '<C-k>', '<Up>') -- Move cursor up 
+vim.keymap.set('i', '<C-l>', '<Right>') -- Move cursor right
+
+vim.keymap.set('i', '<C-d>', '<Del>') -- Delete key
+
+-- Misc.
+vim.keymap.set('n', '<leader>', ':e ++ff=dos<Enter>') -- Clear the ^M's after paste
+vim.keymap.set('n', 'tg', 'gT') -- Reverse switch tabs
+
+-- Telescope
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {}) -- Find files
+vim.keymap.set('n', '<leader>fg', builtin.git_files, {}) -- Fuzzy finding (search through git)
