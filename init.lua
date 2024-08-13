8a9264f626f3945308b5e90ac95666b269121921
Michael Barrientos <michaelbarrientos788@gmail.com>
2024-08-13 13:41:10 -0400

    Added treesitter

--- init.lua
+++ init.lua
@@ -1,6 +1,9 @@
 -- Lazy
 require("config.lazy")
 
+-- Treesitter
+require("lazy").setup({{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}})
+
 -- Options
 require("mike.core.options")
 require("mike.core.keymaps")
