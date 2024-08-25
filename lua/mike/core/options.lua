c0b01157a9a3b6c50e8095a2e44330bc099d174b
Michael Barrientos <michaelbarrientos788@gmail.com>
2024-08-25 17:10:12 -0400

    chore: removed unnecessary options

--- lua/mike/core/options.lua
+++ lua/mike/core/options.lua
@@ -14,23 +14,15 @@ opt.autoindent = true
 -- line wrapping
 opt.wrap = true
 
---search settings
-opt.ignorecase = true
-opt.smartcase = true
-
 -- cursor line
 opt.cursorline = true
 
 -- appearance
 opt.termguicolors = true
-opt.background = "dark"
 opt.signcolumn = "yes"
 
 -- backspace
 opt.backspace = "indent,eol,start"
-
--- clipboard
--- opt.clipboard:append("unnamedplus")
 
 -- split windows
 opt.splitright = true
